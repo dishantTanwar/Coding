924ae5a13bc75c689731c47417eda41d9edb869c
pirateKing <dishanttanwar@gmail.com>
2021-02-02 23:43:45 +0530

    Modify Selection Sort

--- java/SelectionSort.java
+++ java/SelectionSort.java
@@ -18,6 +18,27 @@ class SelectionSort {
         for(int i = 0; i < input.length; i++) {
             System.out.print(input[i] + " ");
         }
+        System.out.println();
+    }
+
+    // Better
+    // 3 7 4 2 5 3 1 
+    public static int[] selectionSortBetter(int[] input) {
+        int minIdx = -1;
+        for (int i = 0; i < input.length - 1; i++) {
+
+            int min = Integer.MAX_VALUE;
+            for (int j = i; j < input.length; j++) {
+                if (input[j] < min) {
+                    min = input[j];
+                    minIdx = j;
+                }
+            }
+            int temp = input[i];
+            input[i] = min;
+            input[minIdx] = temp;
+        }
+        return input;
     }
     public static void main(String[] args) {
         System.out.println("Hello, this program performs a Selection Sort.");
@@ -31,7 +52,11 @@ class SelectionSort {
         for(int i=0; i < size; i++) {
             input[i]  = scanner.nextInt();                
         }
+        System.out.println("Selection sort normal");
         print(selectionSort(input));
+        System.out.println("Selection sort better");
+        print(selectionSort(input));
+
         scanner.close();    
     }   
     
